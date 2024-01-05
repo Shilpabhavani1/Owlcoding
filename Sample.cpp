8c026d94d56e96ce55e76a470f31ede1a66cbbc0
Bhavani <96565325+Shilpabhavani1@users.noreply.github.com>
2024-01-05 22:24:59 +0530

    Create Sample.cpp

---
+++ Sample.cpp
@@ -0,0 +1,11 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    int a=10,b=20,c=30;
+    // std::cout << "Hello world!";
+    cout<< a+b+c;
+
+    return 0;
+}
